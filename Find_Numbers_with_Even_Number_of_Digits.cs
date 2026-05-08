423ec43ed14ef26340a3b438b0b60eea171c5f73
JanP26 <jan.pirog@microsoft.wsei.edu.pl>
2026-05-08 14:25:42 +0200

    Find_Numbers_with_Even_Number_of_Digits.cs

---
+++ Find_Numbers_with_Even_Number_of_Digits.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int FindNumbers(int[] nums) {
+        int count = 0;
+
+        foreach( int n in nums)
+           {
+            if (n.ToString().Length % 2 == 0)
+            {
+            count++;
+            }
+        }
+        return count;
+        
+        
+        
+            
+    }
+    
+}
