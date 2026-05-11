1d39f1afe87ce312a0d5b8722ca95696a45eddb6
JanP26 <jan.pirog@microsoft.wsei.edu.pl>
2026-05-11 13:16:03 +0200

    Valid_Mountain_Array.cs

--- Valid_Mountain_Array.cs
+++ Valid_Mountain_Array.cs
@@ -11,6 +11,10 @@ public class Solution {
         if( n < 3) 
         return false;
 
+        
+        if(i == 0 || i == n -1)
+        return false;
+
     
 
         while(i + 1 < n && arr[i] > arr[i +1])
